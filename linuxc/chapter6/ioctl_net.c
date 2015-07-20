9a15dbe974a5927e0a5d31592fe4419c6329a38e
dream2018 <zhoupans_mail@163.com>
2015-07-20 16:10:11 +0800

    TEST

--- linuxc/chapter6/ioctl_net.c
+++ linuxc/chapter6/ioctl_net.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 #include<unistd.h>
 #include<sys/ioctl.h>
-#include<stdib.h>
+#include<stdlib.h>
 #include<sys/socket.h>
 #include<netinet/in.h>
 #include<arpa/inet.h>
 #include<sys/types.h>
 #include<net/if.h>
+#include<string.h>
 
 unsigned char g_eth_name[16];
 unsigned char g_macaddr[6];
@@ -18,16 +19,17 @@ void init_net()
 {
     int i;
     int sock;
-    struct socket_in sin;
+    struct sockaddr_in sin;
     struct ifreq ifr;
 
-    sock=socket(AF_IENT,SOCK_DGRAM ,0);
+    sock=socket(AF_INET,SOCK_DGRAM ,0);
     if(sock==0)
     {
         perror("socket");
     }
     strcpy(g_eth_name ,"eth0");
     strcpy(ifr.ifr_name,g_eth_name);
+    printf("eth name:\t%s\n",g_eth_name);
 
     if(ioctl(sock,SIOCGIFHWADDR,&ifr)<0)
     {
@@ -42,17 +44,25 @@ void init_net()
     }
     printf("%.2x\n",g_macaddr[i]);
 
-    if(iocal(sock,SIOCGIFADDR,&ifr)<0)
+    if(ioctl(sock,SIOCGIFADDR,&ifr)<0)
     {
-        perror("ioctl");
+       // perror("ioctl");
     }
     memcpy(&sin,&ifr.ifr_addr,sizeof(sin));
     g_ipaddr=sin.sin_addr.s_addr;
-    printf("local eth0 :\t%s\n",ient_ntoa(sin.sin_addr));
+    printf("local eth0 :\t%s\n",inet_ntoa(sin.sin_addr));
 
     if(ioctl(sock,SIOCGIFBRDADDR,&ifr)<0)
     {
-        perror("ioctl");
+       // perror("ioctl");
+    }
+    memcpy(&sin,&ifr.ifr_addr,sizeof(sin));
+    g_broadcast_ipaddr=sin.sin_addr.s_addr;
+    printf("broadcast:\t%s\n",inet_ntoa(sin.sin_addr));
+
+    if(ioctl(sock,SIOCGIFNETMASK,&ifr)<0)
+    {
+       // perror("ioctl");
     }
     memcpy(&sin,&ifr.ifr_addr,sizeof(sin));
     g_subnetmask=sin.sin_addr.s_addr;
