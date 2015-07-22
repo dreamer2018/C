348bfb2c2eb2852f3d7c0d9c1698d1c508566495
dream2018 <zhoupans_mail@163.com>
2015-07-22 08:54:33 +0800

    linked_2.c first commit

--- DataStructures/chapter2/linked_2.c
+++ DataStructures/chapter2/linked_2.c
@@ -7,7 +7,54 @@ typedef struct student
     struct student *next;
     struct student *pre;
 }stu;
-void main()
+void Head()
+{
+    int a;
+    float b;
+    stu *New,*Old,*head,*p,*end;
+    New=Old=(stu *)malloc(sizeof(stu));
+    printf("Please Input:");
+    scanf("%d %f",&a,&b);
+    getchar();
+    if(0!=a)
+    {
+        New->id=a;
+        New->score=b;
+        New->next=NULL;
+        end=New;
+    }
+    while(Old->id!=0)
+    {
+        New=(stu *)malloc(sizeof(stu));
+        printf("Please Input:");
+        scanf("%d %f",&a,&b);
+        getchar();
+        if(a!=0)
+        {
+            New->id=a;
+            New->score=b;
+            head=New;
+            New->next=Old;
+            Old->pre=New;
+            Old=New;
+        }
+        else
+        {
+            free(New);
+            break;
+        }
+    }
+    p=end;
+   // p=head;
+    while(p!=NULL)
+    {
+        printf("id :%d\t",p->id);
+        printf("score:%0.2f\n",p->score);
+       // p=p->next;
+        p=p->pre;
+    }
+}
+void Tail()
 {
     int a;
     float b;
@@ -52,4 +99,9 @@ void main()
         printf("score:%0.2f\n",p->score);
         p=p->pre;
     }
+}
+void main()
+{
+   Head();
+   //Tail();
 }
