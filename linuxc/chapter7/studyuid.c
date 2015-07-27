24c9a98fa369e5c90ec74a2a70d6d90afb68d3eb
dream2018 <zhoupans_mail@163.com>
2015-07-27 15:13:46 +0800

    test

---
+++ linuxc/chapter7/studyuid.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<errno.h>
+#include<stdlib.h>
+#include<string.h>
+extern int errno;
+int main()
+{
+    int fd;
+    printf("uid study:\n");
+    printf("Process's uid=%d,euid=%d\n",getuid(),geteuid());
+    if((fd=open("test.c",O_RDWR))==-1)
+    {
+        printf("Open failure ,errno is %d:%s \n",errno,strerror(errno));
+        exit(1);
+    }
+    else
+    {
+        printf("Open successfully!\n");
+    }
+    close(fd);
+    exit(0);
+}
