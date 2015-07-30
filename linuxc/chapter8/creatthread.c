b933ff5a5c5c5740d7d9eef631c5c3afcffba4ae
dream2018 <zhoupans_mail@163.com>
2015-07-30 09:13:50 +0800

    creatthread.c

---
+++ linuxc/chapter8/creatthread.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<pthread.h>
+
+int *thread(void *arg)
+{
+    pthread_t newthid;
+    newthid=pthread_self();
+    printf("This thread id is %d",newthid);
+    return NULL;
+}
+int main(void)
+{
+    pthread_t thid;
+
+    printf("this thread id is %u",pthread_self());
+    if(pthread_create(&thid,NULL,(void *)thread,NULL)!=0)
+    {
+        printf("thread creat fail !\n");
+        exit(1);
+    }
+    sleep(1);
+    exit(0);
+}
