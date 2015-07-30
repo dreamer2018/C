c05693fcd4a014941bce18acb91106b07c367c29
dream2018 <zhoupans_mail@163.com>
2015-07-30 21:59:17 +0800

    tsd_test.c

--- linuxc/chapter8/tsd_test.c
+++ linuxc/chapter8/tsd_test.c
@@ -26,6 +26,6 @@ int main()
     pthread_create(&thid,NULL,thread,NULL);
     sleep(3);
     pthread_setspecific(key, (void *)&tsd);
-    printf("%d\n",pthread_getspecific(key));
+    printf("%lu\n",pthread_getspecific(key));
     pthread_key_delete(key);
 }
