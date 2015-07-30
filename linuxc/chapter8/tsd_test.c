7ffbe207d59716a8e08e1e03fd1d0d998c1c1287
dream2018 <zhoupans_mail@163.com>
2015-07-30 19:06:46 +0800

    tsd_test.c

---
+++ linuxc/chapter8/tsd_test.c
@@ -0,0 +1,29 @@
+/*************************************************************************
+	> File Name: tsd_test.c
+	> Author: ZhouPan / github:dreamer2018 
+	> Blog: blog.csdn.net/it_dream_er 
+	> Mail: zhoupans_mail@163.com 
+	> Created Time: Thu 30 Jul 2015 05:29:03 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+#include<unistd.h>
+#include<pthread.h>
+
+pthread_key_t key;
+
+void pthread( void * arg )
+{
+    int tsd = 5 ;
+    pthread_setspecific( key ,(void *)&tsd );
+    printf("%36d \n",pthread_getspecific(key));
+}
+int main()
+{
+    pthread_t thid;
+    pthread_key_create( &key ,(void *)pthread);
+    pthread_create(&thid,NULL,(void *)pthread,NULL);
+    sleep(5);
+    pthread_key_delete(key);
+}
+
