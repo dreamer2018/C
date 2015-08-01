3856276ae7ed964d8d76fafc74f65b0c807c46f1
dream2018 <zhoupans_mail@163.com>
2015-08-01 18:25:54 +0800

    perror_test.c

---
+++ linuxc/chapter8/perror_test.c
@@ -0,0 +1,34 @@
+/*************************************************************************
+	> File Name: checkerror.c
+	> Author: ZhouPan / github:dreamer2018 
+	> Blog: blog.csdn.net/it_dream_er 
+	> Mail: zhoupans_mail@163.com 
+	> Created Time: Sat 01 Aug 2015 05:37:05 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+#include<errno.h>
+#include<string.h>
+
+
+int main()
+{
+    FILE *fp;
+
+    errno=0;
+    char a[100];
+    fp=fopen("2","r");
+
+    if(fp==NULL)
+    {
+        //printf("文件打开失败！errno=%d\n",errno);  //文件不存在，errno=2，文件无读权限，errno=13
+        strcpy(a,"文件打开失败！");
+        perror(a);
+    }
+    else
+    {
+        printf("文件打开成功！errno=%d\n",errno);
+        fclose(fp);
+    }
+}
+
