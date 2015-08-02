93eec3a85af8d64f89064f2e8532f6ae775b89a7
dream2018 <zhoupans_mail@163.com>
2015-08-02 19:07:39 +0800

    doub_pipe.c

--- linuxc/chapter10/doub_pipe.c
+++ linuxc/chapter10/doub_pipe.c
@@ -12,23 +12,28 @@
 #include<sys/types.h>
 #include<string.h>
 #include<sys/stat.h>
+#include<wait.h>
+
 
 void pipe_rw_1(int fd1,int fd2)
 {
-    char *message_w="this is duoble pipe_1 for write";
-    char *message_r;
-    message_r=(char *)malloc(100*sizeof(char));
+    char *message_w="this is duoble pipe_1 for write\n";
+    char message_r[100];
+
     read(fd1,message_r,100);
     write(fd2,message_w,strlen(message_w)+1);
+    
+   // read(fd1,message_r,100);
     printf(" 1:get message is %s\n",message_r);
 }
 void pipe_rw_2(int fd1,int fd2)
 {
-    char *message_w="this is duoble pipe_2 for write";
-    char *message_r;
-    message_r=(char *)malloc(100*sizeof(char));
-    read(fd1,message_r,100);
+    char *message_w="this is duoble pipe_2 for write\n";
+    char message_r[100];
+    
     write(fd2,message_w,strlen(message_w)+1);
+    
+    read(fd1,message_r,100);
     printf(" 2:get message is %s\n",message_r);
 }
 int main()
@@ -36,7 +41,9 @@ int main()
     int fd1[2],fd2[2];
     int stat_val;
     pid_t pid;
-    if(pipe(fd2))
+    
+    printf("this doub pipe test\n");
+    if(pipe(fd1))
     {
         printf("pipe 1 create fail\n");
         return 0;
@@ -51,18 +58,17 @@ int main()
     {
         case -1:
             printf("create new process fail\n");
-            return 0;
-        case 0:
-            close(fd1[1]);
-            close(fd2[0]);
-            pipe_rw_1(fd1[0],fd2[1]);
             exit(1);
-        default :
+        case 0:
             close(fd1[0]);
             close(fd2[1]);
-            pipe_rw_2(fd2[0],fd1[1]);
+            pipe_rw_1(fd2[0],fd1[1]);
+            exit(0);
+        default:
+            close(fd1[1]);
+            close(fd2[0]);
+            pipe_rw_2(fd1[0],fd2[1]);
             wait(&stat_val);
-            exit(1);
-            
+            exit(0);
     }
 }
