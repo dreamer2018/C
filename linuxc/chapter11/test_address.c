3f97448281a8fdf0b0191ad6f650bb2a2b62c353
dreamer2018 <zhoupans_mail@163.com>
2015-08-04 21:08:36 +0800

    test_address.c

---
+++ linuxc/chapter11/test_address.c
@@ -0,0 +1,62 @@
+/*************************************************************************
+	> File Name: test_address.c
+	> Author: ZhouPan / github:dreamer2018 
+	> Blog: blog.csdn.net/it_dream_er 
+	> Mail: zhoupans_mail@163.com 
+	> Created Time: Tue 04 Aug 2015 08:45:22 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+
+int main()
+{
+    char buffer[32];
+    int ret=0;
+    int host=0;
+    int network=0;
+    unsigned int address=0;
+    
+    struct in_addr in;
+    in.s_addr=0;
+
+    printf("请输入你的ip地址：");
+    fgets(buffer,31,stdin);
+    buffer[31]='\0';
+
+    if(!(ret=inet_aton(buffer,&in)))
+    {
+        printf("inet_aton:\t invalid address\n");
+    }
+    else
+    {
+        printf("inet_aton:0x%x",in.s_addr);
+    }
+
+    if((address=inet_addr(buf))==-1)
+    {
+        printf("inet_aton:\t invalid address\n");
+    }
+    else
+    {
+        printf("inet_aton:0x%x",address);
+    }
+
+    if((address=inet_network(buffer))==-1)
+    {
+        printf("inet_aton:\t invalid address\n");
+    }
+    else
+    {  
+        printf("inet_aton:0x%x",address);
+    }
+
+    if(inet_ntoa(in)==NULL)
+    {
+        
+    }
+}
