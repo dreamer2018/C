6f7797472a7fc998925b606fde886d1069f60aa2
dreamer2018 <zhoupans_mail@163.com>
2015-08-05 19:35:11 +0800

    Client.c

--- linuxc/chapter11/Project/Client.c
+++ linuxc/chapter11/Project/Client.c
@@ -22,36 +22,174 @@
 
 int my_recv(int conn_fd,char *data_buf,int len)  //
 {
-   // char buf[BUFMAX];
-    int sign;
-    sign=recv(conn_fd,data_buf,BUFMAX,len);
-    if(sign<0)
+    char recv_buf[BUFMAX];
+    char *pread;
+    int len_remain=0;
+    int i;
+    if(len_remain<=0)
+    {
+        if((len_remain=recv(conn_fd,recv_buf,sizeof(recv_buf),0))<0)
+        {
+            perror("recv");
+        }
+        else if(len_remain==0) //目的计算机的socket连接关闭
+        {
+            return 0;
+        }
+        pread=recv_buf; //重新初始化pread
+    }
+    for(i=0;*pread!='\n';i++)
+    {
+        if(i>len)
+        {
+            return -1;
+        }
+        data_buf[i]=*pread++;
+        len_remain--;
+    }
+    //去除结束标志
+    len_remain--;
+    pread++;
+
+    return i;    //读取成功
+}
+
+int get_userinfo(char *buf,int len)     //获取用户输入，存到buf中buf长度为len
+{
+    int i;
+    /*
+    if(buf==NULL)
     {
         return -1;
     }
-    else if(sign==0)
+    */
+
+    i=0;
+    while(i<len)
     {
-        return -2;
+        buf[i]=getchar();
+        if(buf[i]=='\n')
+        {
+            buf[i++]='\0';
+            return i;
+        }
+        i++;
     }
-    else
-    {
-        return sign;
-    }
-}
- 
-int get_userinfo(char *buf,int len)     //获取用户输入，存到buf中buf长度为len
-{
-    
 }
 
 void input_userinfo(int conn_fd,char *string) //输入用户名，通过conn_fd 发出
 {
+    char input_buf[32];
+    char recv_buf[BUFMAX];
+    int flag_userinfo;
 
+    do
+    {
+        printf(" %s:",string);
+        if(get_userinfo(input_buf,32)<0)
+        {
+            printf("error return from get_userinfo \n");
+            exit(1);
+        }
+        
+        if(send(conn_fd,input_buf,strlen(input_buf),0)<0)
+        {
+            perror("send");   
+        }
+        
+        if(my_recv(conn_fd,recv_buf,sizeof(recv_buf))<0)
+        {
+            printf("data to long\n");
+            exit(1);
+        }
+        
+        if(recv_buf[0]==VALID_USERINFO)
+        {
+            printf(" %s error ,input again ",string);
+            flag_userinfo=VALID_USERINFO;
+        }
+    }while(flag_userinfo==INVALID_USERINFO);
 }
 
+int main(int argc,char *argv[])
+{   
+    int i;
+    int ret;
+    int conn_fd;
+    int serv_port;
+    struct sockaddr_in serv_addr;
+    char recv_buf[BUFMAX];
 
-int main()
-{
+    if(argc!=5)
+    {
+        printf("Usage: [-p] [serv_port] [-a] [serv_address] \n");
+        exit(1);
+    }
+    //初始化服务器端地址结构
+    memset(&serv_addr,0,sizeof(struct sockaddr_in));
+    serv_addr.sin_family=AF_INET;
+    //从命令行获取服务器端的端口和地址
+    for(i=0;i<argc;i++)
+    {
+        if(strcmp("-p",argv[i])==0)
+        {
+            serv_port=atoi(argv[i+1]);  //将字符串转换成整型
+            if(serv_port<0||serv_port>65535)
+            {
+                printf("invalid serv_addr.sin_port\n");
+                exit(1);
+            }
+            else
+            {
+                serv_addr.sin_port=htons(serv_port);
+            }
+            continue;
+        }
+        
+        if(strcmp("-a",argv[i])==0)
+        {
+            if(inet_aton(argv[i+1],&serv_addr.sin_addr)==0)
+            {
+                printf("invalid server ip address\n");
+                exit(1);
+            }
+            continue;
+        }
+    }
+
+    //检测数否少输入了某项参数
+    if(serv_addr.sin_port==0||serv_addr.sin_addr.s_addr==0)
+    {
+        printf("Usage :[-p] [serv_addr.sin_port] [-a] [serv_address]\n");
+        exit(1);
+    }
+    conn_fd=socket(AF_INET,SOCK_STREAM,0);
+    if(conn_fd<0)
+    {
+        perror("socket");
+    }
     
+    if(connect(conn_fd,(struct sockaddr *)&serv_addr,sizeof(struct sockaddr))<0)
+    {
+        perror("connect");
+    }
+
+    input_userinfo(conn_fd,"Username");
+    input_userinfo(conn_fd,"Password");
+
+    //读取欢迎信息并打印
+    if((ret=my_recv(conn_fd,recv_buf,sizeof(recv_buf)))<0)
+    {
+        printf("data is too long \n");
+        exit(1);
+    }
+    for(i=0;i<ret;i++)
+    {
+        printf("%c",recv_buf[i]);
+    }
+    printf("\n");
+
+    close(conn_fd);
+    return 0;
 }
 
