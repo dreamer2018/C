651029699cf57357caaf2622a9c7fdfb4f59b9fd
dreamer2018 <zhoupans_mail@163.com>
2015-08-06 22:28:25 +0800

    Service_New.c

---
+++ linuxc/chapter11/Project/Service_New.c
@@ -0,0 +1,91 @@
+/*************************************************************************
+	> File Name: Service.c
+	> Author: ZhouPan / github:dreamer2018 
+	> Blog: blog.csdn.net/it_dream_er 
+	> Mail: zhoupans_mail@163.com 
+	> Created Time: Wed 05 Aug 2015 10:21:26 AM CST
+ ************************************************************************/
+
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<netinet/in.h>
+#include<string.h>
+#include<arpa/inet.h>
+#include<error.h>
+#include<sys/time.h>
+
+#define SERV_PORT 8080
+#define MAX_LIST 10
+#define USERNAME 0
+#define PASSWORD 1
+#define BUFMAX 1024
+
+int main()
+{
+    int sock_fd;
+    int conn_fd;
+    int optval;
+    int srv_len,clt_len;
+    int ret;
+    int flag_recv=0;
+    int switch_num;
+    pid_t pid;
+    struct sockaddr_in srv_sock,clt_sock;
+    char recv_buf[BUFMAX];
+    
+    fd_set readfds,testfds;
+    
+     //创建一个TCP 套接字
+    sock_fd=socket(AF_INET,SOCK_STREAM,0);
+    if(sock_fd<0)
+    {
+        perror("socket");
+    }
+
+    //设置该套接字使之可以重新绑定
+    optval=1;
+    
+    if(setsockopt(sock_fd,SOL_SOCKET,SO_REUSEADDR,&optval,sizeof(int))<0)
+    {
+        perror("setsockopt");
+    }
+    
+    //初始化本地端
+    memset(&srv_sock,0,sizeof(struct sockaddr_in ));
+    
+    srv_sock.sin_family=AF_INET;
+    srv_sock.sin_port=htons(SERV_PORT);
+    srv_sock.sin_addr.s_addr=htonl(INADDR_ANY);
+    
+    //绑定套接字到本地端
+    if(bind(sock_fd,(struct sockaddr *)&srv_sock,sizeof(struct sockaddr_in ))<0)
+    {
+        perror("bind");
+    }
+    
+    //设置监听
+    if(listen(sock_fd,MAX_LIST)<0)
+    {
+        perror("listen");
+    }
+
+    srv_len=sizeof(struct sockaddr_in);
+    
+    FD_ZERO(&readfds);
+    FD_SET(sock_fd,&readfds);
+    while(1)
+    {
+        char ch;
+        int fd;
+        int nread;
+        
+        testfds=readfds;
+        
+        printf("service waiting\n");
+        ret=select(FD_SETSIZE , &testfds ,(fd_set *)0,(fd_set *)0,(struct timeval *)0);
+        
+    }
+}
