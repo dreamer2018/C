4590fdbc429a4d3110e5c6dfe3d442aee9fdd481
dreamer2018 <zhoupans_mail@163.com>
2015-08-06 14:40:48 +0800

    Service.c

--- linuxc/chapter11/Project/Service.c
+++ linuxc/chapter11/Project/Service.c
@@ -24,11 +24,44 @@
 #define INYALLD_USERINFO 'n'
 #define VALID_USERINFO 'y'
 
+//链表头插法初始化
+
+
+#define List_Init(list,list_node_t){                    \
+    list=(list_node_t *)malloc(sizeof(list_node_t));    \
+    list->next=NULL;                                    \
+} 
+
+//链表头插法宏定义
+
+#define List_AddHead(list,newNode){         \
+    newNode->next=list->next;               \
+    list->next=newNode;                     \
+}
+
+//用户信息结构体
 struct user
 {
     char name[40];
     char password[40];
 };
+
+//上线用户信息保存
+
+typedef struct online
+{
+    char username[40];
+    int sock_fd;
+    struct online *next;
+} online_node_t;
+
+online_node_t *online_head;
+
+//创建用户在线信息的单向链表
+
+
+//链表初始化
+
 
 struct user users[]={{"admin","admin"},{"guest","guest"},{" "," "}};
 
@@ -56,10 +89,36 @@ int find_name(char *name) //出错返回-2，找不到用户名返回-1，找到
 
 void send_data(int conn_fd,char *string)
 {
+
     if(send(conn_fd,string,strlen(string),0)<0)
     {
         perror("send");
+    }       
+}
+
+void send_data_to(char *string)
+{
+    online_node_t *p;
+    p=online_head->next;
+    while(p!=NULL)
+    {
+        send_data(p->sock_fd,string);
+        p=p->next;
     }
+}
+
+void On_Info_Add(char *str,int sock_fd)
+{
+    online_node_t *p;
+    p=(online_node_t *)malloc(sizeof(online_node_t));
+    strcpy(p->username,str);
+    p->sock_fd=sock_fd;
+    printf("Test_1:%s %d\n",p->username,p->sock_fd);
+    //List_AddHead(online_head,p);         
+    p->next=online_head->next;               
+    printf("test__\n");
+    online_head->next=p;                     
+    printf("Test_2:%s %d\n",p->username,p->sock_fd);
 }
 int main()
 {
@@ -72,6 +131,8 @@ int main()
     int switch_num;
     pid_t pid;
     struct sockaddr_in srv_sock,clt_sock;
+    online_node_t *p; 
+    List_Init(p,online_node_t);
     char recv_buf[BUFMAX];
 
     //创建一个TCP 套接字
@@ -84,7 +145,6 @@ int main()
     //设置该套接字使之可以重新绑定
     optval=1;
     
-    //len=sizeof(optval);
     if(setsockopt(sock_fd,SOL_SOCKET,SO_REUSEADDR,&optval,sizeof(int))<0)
     {
         perror("setsockopt");
@@ -96,20 +156,11 @@ int main()
     srv_sock.sin_family=AF_INET;
     srv_sock.sin_port=htons(SERV_PORT);
     srv_sock.sin_addr.s_addr=htonl(INADDR_ANY);
-   
-   /*memset(sock.sin_zero,0,sizeof(sock.sin_zero));
-    conn_fd=connect(sock_fd,(struct sockaddr *)&srv_sock,sizeof(struct sockaddr_in));
-    if(conn_fd<0)
-    {
-        perror("connect");
-    }
-    */
     
     //绑定套接字到本地端
     if(bind(sock_fd,(struct sockaddr *)&srv_sock,sizeof(struct sockaddr_in ))<0)
     {
         perror("bind");
-
     }
     
     //设置监听
@@ -129,6 +180,8 @@ int main()
             perror("accept");
         }
         printf("accept a new client ip: %s \n",inet_ntoa(clt_sock.sin_addr));
+        
+        
         
         //创建一个子进程用于接受客户端连接请求
         if((pid=fork())==0)
@@ -164,9 +217,15 @@ int main()
                 {
                     if(!strcmp(users[switch_num].name,recv_buf))
                     {
+                        //On_Info_Add(users[switch_num].name,conn_fd);
+                        p=(online_node_t *)malloc(sizeof(online_node_t));
+                        strcpy(p->username,users[switch_num].name);
+                        p->sock_fd=conn_fd;
+                        //List_AddHead(online_head,p);
                         send_data(conn_fd,"y\n");
                         send_data(conn_fd,"welcome login my TCP service\n");
                         printf("%s login\n",users[switch_num].name);
+                        //On_Info_Add(users[switch_num].name,conn_fd);
                         break;
                     }
                     else
