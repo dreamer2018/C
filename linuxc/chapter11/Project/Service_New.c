6ca5d28b57445b066377ced1d96286367d752196
dreamer2018 <zhoupans_mail@163.com>
2015-08-07 09:27:48 +0800

    Service_New.c

--- linuxc/chapter11/Project/Service_New.c
+++ linuxc/chapter11/Project/Service_New.c
@@ -28,6 +28,7 @@ int main()
 {
     int sock_fd;
     int conn_fd;
+    int fd_count=0;
     int optval;
     int srv_len,clt_len;
     int ret;
@@ -75,7 +76,7 @@ int main()
     }
     fd_list[0]=sock_fd;
     srv_len=sizeof(struct sockaddr_in);
-    
+    fd_count++;
     FD_ZERO(&readfds);
     FD_SET(sock_fd,&readfds);
     while(1)
@@ -92,15 +93,17 @@ int main()
             perror("select");
         }
         
-        for(fd=0;fd<(MAX_LIST+4);fd++)
+        for(fd=0;fd<fd_count;fd++)
         {
             if(FD_ISSET(fd_list[fd],&testfds))
             {
-                if(fd==sock_fd)
+                if(fd_list[fd]==sock_fd)
                 {
                     clt_len=sizeof(struct sockaddr_in);
                     conn_fd=accept(sock_fd,(struct sockaddr *)&clt_sock,&clt_len);
                     FD_SET(conn_fd,&readfds);
+                    fd_list[fd_count]=conn_fd;
+                    fd_count++;
                     printf("adding client on fd %d\n",conn_fd);
                 }
                 else
