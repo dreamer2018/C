e18521bdc962490d6b6fbd72fe2fce1c6bada10e
dreamer2018 <zhoupans_mail@163.com>
2015-08-09 10:02:44 +0800

    Service_0.2

--- linuxc/chapter11/Project/Service.c
+++ linuxc/chapter11/Project/Service.c
@@ -18,19 +18,35 @@
 #include<sys/time.h>
 #include<sys/ioctl.h>
 
-#define SERV_PORT 8000
-#define MAX_LIST 10
-#define USERNAME 0
-#define PASSWORD 1
-#define BUFSIZE 1024
 
-typedef struct On_Line
+/*
+ * List为自定义的头文件，里面包含了对单向链表的操作：
+ * List_Init(head,list_node_t)  单向链表初始化宏定义
+ * List_AddHead(head,newNode)   单项链表头插发宏定义
+*/
+#include<List.h>
+
+
+//宏定义
+
+#define SERV_PORT 8080      //端口
+#define MAX_LIST 10         //最长等待序列
+#define BUFSIZE 1024        //缓冲区大小
+
+/*
+ *  在线用户链表  
+*/
+
+typedef struct On_Line     
 {
-    char name[21];
-    int sock_fd;
+    char name[21];    //在线的用户名
+    int sock_fd;      //在线用户套接字描述符
     struct On_Line *next;
-}
+} online_node_t;
 
+//在线用户链表头结点（全局变量）
+
+online_node_t *head;
 
 int main()
 {
@@ -46,6 +62,7 @@ int main()
     pid_t pid;
     struct sockaddr_in srv_sock,clt_sock;
     char recv_buf[BUFSIZE];
+    List_Init(head,online_node_t);
     memset(fd_list,0,MAX_LIST);  
     fd_set readfds,testfds;
     
@@ -86,7 +103,7 @@ int main()
     fd_list[fd_count]=sock_fd;
     srv_len=sizeof(struct sockaddr_in);
     fd_count++;
-    FD_ZERO(&readfds);
+    FD_ZERO(&readfds); 
     FD_SET(sock_fd,&readfds);
     while(1)
     {
@@ -105,24 +122,25 @@ int main()
         
         for(fd=0;fd<fd_count;fd++)
         {
-            if(FD_ISSET(fd_list[fd],&testfds))
+            if(FD_ISSET(fd_list[fd],&testfds))  //检测出现响应的在不在在线用户链表中，不在责不进行操作，直接跳过
             {
-                if(fd_list[fd]==sock_fd)
+                if(fd_list[fd]==sock_fd) //如果响应的是监听套接字，则说明是一个新的用户请求
                 {
                     clt_len=sizeof(struct sockaddr_in);
                     conn_fd=accept(sock_fd,(struct sockaddr *)&clt_sock,&clt_len);
+                                                //使用vfork创建一个进程，用于进行密码注册用户或登录验证
                     FD_SET(conn_fd,&readfds);  
                     fd_list[fd_count]=conn_fd;
                     fd_count++;
                     printf("adding client on fd %d\n",conn_fd);
                 }
-                else
+                else  //否则，表示是已在线的用户出现操作
                 {
-                    memset(recv_buf,0,sizeof(recv_buf));
-                    nread = recv(fd_list[fd],recv_buf,sizeof(recv_buf),0);
-                    if(nread==0)
+                    memset(recv_buf,0,sizeof(recv_buf));  //每次先将缓冲区清空
+                    nread = recv(fd_list[fd],recv_buf,sizeof(recv_buf),0); //接受用户发送来的数据
+                    if(nread==0) //如果数据长度为零，则表示用户离开
                     {
-                        close(fd_list[fd]);
+                        close(fd_list[fd]);                
                         FD_CLR(fd_list[fd],&readfds);
                         printf("removeing clinet on fd %d\n",fd);
                         if(fd!=fd_count)
@@ -134,9 +152,9 @@ int main()
                         }
                         fd_count--;
                     }
-                    else
+                    else 
                     {
-                        if((pid=fork())==0)
+                        if((pid=fork())==0)  //创建一个新的进程，用于处理用户发来的信息，解析并转发给其他用户
                         {
                             printf("Tets fd_count=%d\n",fd_count);
                             for(j=1;j<fd_count;j++)
