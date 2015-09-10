2615046c2069d93b8d16af87188c3166a3926e4d
dreamer2018 <zhoupans_mail@163.com>
2015-09-10 22:34:47 +0800

    add

--- DataStructures/chapter2/homework/2.2.5.c
+++ DataStructures/chapter2/homework/2.2.5.c
@@ -1,46 +1,109 @@
 /*************************************************************************
-	> File Name: 2.2.5.c
+	> File Name: linked_1.c
 	> Author: ZhouPan / github:dreamer2018 
 	> Mail: zhoupans_mail@163.com
 	> Blog: blog.csdn.net/it_dream_er
-	> Created Time: Wed 09 Sep 2015 07:18:35 PM CST
+	> Created Time: Mon 07 Sep 2015 04:00:50 PM CST
  ************************************************************************/
 
 #include<stdio.h>
 #include<malloc.h>
 
-typedef int LinkType;
-typedef struct Example
+typedef struct student
 {
-    LinkType i;
-    struct Example *next;
-}Linklist;
+    int id;
+    float score;
+    struct student *next;
+}stu;
 
-void Init(Linklist *h)
+stu *Tail()
 {
-    h=(Linklist *)malloc(sizeof(Linklist));
-    h->next=h;
-}
-
-Linklist *Insert()
-{
-    int l;
-    Linklist *p,*q,*r;
-    p=(Linklist *)malloc(sizeof(Linklist));
-    scanf("%d",l);
-    p->i=l;
-
-    while(1)
+    int id;
+    float score;
+    stu *head,*p,*q;
+    head=(stu *)malloc(sizeof(stu));
+    scanf("%d %f",&id,&score);
+    head->id=id;
+    head->score=score;
+    p=head;
+    while(p->id>0)
     {
-        q=(Linklist *)malloc(sizeof(Linklist));
-        q->i=l;
-        q->p
+        scanf("%d %f",&id,&score);
+        getchar();
+        if(id<=0)
+        {
+            break;
+        }
+        q=(stu *)malloc(sizeof(stu));
+        q->id=id;
+        q->score=score;
+        p->next=q;
+        p=q;
     }
-    q->next=p;
-
+    p->next=head;
+    return head;
 }
- 
+
+int Find(stu *s)
+{
+    if(s==NULL)
+    {
+        printf("Linklist is empty!\n");
+        return 0;
+    }
+    stu *p,*q;
+    p=s;
+    while(p->next->next!=s)
+    {
+        p=p->next;
+    }
+    q=p->next;
+    p->next=s;
+    free(q);
+    return 1;
+}
+
+int Merge(stu *head)
+{
+    if(head==NULL || head->next==NULL)
+    {
+        printf("pos id error\n");
+        return 0;
+    }
+    stu *p,*q;
+    p=head->next;
+    head->next=NULL;
+    while(p)
+    {
+        q=p->next;
+        p->next=head->next;
+        head->next=p;
+        p=q;
+    }
+    return 1;
+}
+
+
 int main()
 {
-     
+    int i=0;
+    stu *head,*p;
+    head=Tail();
+    p=head;
+
+    while(i<4)
+    {
+        i++;
+        printf("%d %f\n",p->id,p->score);
+        p=p->next;
+    }
+    Find(p->next->next);
+    i=0;
+    while(i<4)
+    {
+        i++;
+        printf("%d %f\n",p->id,p->score);
+        p=p->next;
+    }
+
 }
